c124ea53703aafe81e63227e75672f589ec38d26
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2021-12-08 15:32:08 +0100

    Update tasks.py

--- workshop/tasks.py
+++ workshop/tasks.py
@@ -1,5 +1,10 @@
 # For testing your Docker Z3 setup
 # Proper tasks will be added later
+# 
+# When you see the following output after running the script, everything has been configured correctly:
+# sat
+# []
+#
 from z3 import *
 s = Solver()
 print(s.check())
